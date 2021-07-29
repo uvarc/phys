22fab8e12f5d0c24ebcc51d6f57b86774bbd8de1
Joshua Hoskins <jhoskins@jlab.org>
2021-07-29 12:11:24 -0400

    This update bring a minor change to the way the data is queried via the API as well as the addition of two new API calls.
    - The standard data query form has changed from /<model>/<gpd> -> /api/<model>/<gpd> to better format API calls.
    - Two new calls have been added.
    -- /api/<model>/<gpd>/t/<float(signed=True):t> This gives the unique options for Q2 & xbj as well as min-max information in JSON from.
    -- /api/<model>/<gpd>/xbj/<float:xbj> This gives the unique options for Q2 & t as well as min-max information in JSON form.

--- app/routes.py
+++ app/routes.py
@@ -38,7 +38,7 @@ def index():
         return render_template('index.html', title='Home', form=form)
 
 
-@app.route('/<model>/<gpd>')
+@app.route('/api/<model>/<gpd>')
 def params(model=None, gpd=None):
     df = fm.FemtoMesh('femtomesh/data/models/model_{0}/{1}.csv'.format(model, gpd)).open()
 
@@ -63,6 +63,68 @@ def params(model=None, gpd=None):
             kinematics['t'] = t
         if q2 is not None:
             kinematics['q2'] = q2
+
+        kinematics_array.append(kinematics)
+
+    return jsonify({'kinematics': kinematics_array, 'model': info})
+
+@app.route('/api/<model>/<gpd>/t/<float(signed=True):t>')
+def update_axis_t(model='uva', gpd='GPD_H', t=None):
+    df = fm.FemtoMesh('femtomesh/data/models/model_{0}/{1}.csv'.format(model, gpd)).open()
+    df = df[df.t==t]
+
+    kinematics_array = []
+    info = [
+        {
+            'name': model,
+            'xbj': {
+                 'max': df.xbj.max(),
+                 'min': df.xbj.min()
+                 },
+            'q2': {
+                 'max': df.Q2.max(),
+                 'min': df.Q2.min()
+                 }
+        }
+    ]
+
+    for (_xbj, _q2) in list(itertools.zip_longest(df.xbj.unique(), df.Q2.unique())):
+        kinematics = {}
+        if _xbj is not None:
+            kinematics['xbj'] = _xbj
+        if _q2 is not None:
+            kinematics['q2'] = _q2
+
+        kinematics_array.append(kinematics)
+
+    return jsonify({'kinematics': kinematics_array, 'model': info})
+
+@app.route('/api/<model>/<gpd>/xbj/<float:xbj>')
+def update_axis_xbj(model='uva', gpd='GPD_H', xbj=None):
+    df = fm.FemtoMesh('femtomesh/data/models/model_{0}/{1}.csv'.format(model, gpd)).open()
+    df = df[df.xbj==xbj]
+
+    kinematics_array = []
+    info = [
+        {
+            'name': model,
+            't': {
+                 'max': df.t.max(),
+                 'min': df.t.min()
+                 },
+            'q2': {
+                 'max': df.Q2.max(),
+                 'min': df.Q2.min()
+                 }
+        }
+    ]
+
+    for (_t, _q2) in list(itertools.zip_longest(df.t.unique(), df.Q2.unique())):
+        kinematics = {}
+        if _t is not None:
+            kinematics['t'] = _t
+        if _q2 is not None:
+            kinematics['q2'] = _q2
 
         kinematics_array.append(kinematics)
 
