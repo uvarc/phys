127889994e8b86c22897c819b99afa16082f5c0f
Joshua Hoskins <jhoskins@jlab.org>
2021-08-06 14:25:57 -0400

    Added flask-cor module to fix CORS issue in api.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import Config
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='static')
 app.config.from_object(Config)
+CORS(app)
 
 from app import routes
